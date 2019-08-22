04d191e469816082d07ce728de727aecdca74e34
phichidung <dungpc@sphinx-software.com>
2019-08-22 17:57:03 +0700

    add banner advertisement into home page

---
+++ src/components/Banner/BannerAdvertisement.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import                           '../../css/banner/banner.css';
+
+
+export default class BannerAdvertisement extends Component {
+    render() {
+        return (
+            <div className="banner-advertisement">
+                <div>
+                    <p>JUST DO IT</p>
+                </div>
+            </div>
+        );
+    }
+}
