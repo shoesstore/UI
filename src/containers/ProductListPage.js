5108612f0dd6f8eede5060bc89b379bbad9a1f75
vnduy01 <duynt@sphinx-software.com>
2019-08-22 14:58:51 +0700

    modified ProductListPage

--- src/containers/ProductListPage.js
+++ src/containers/ProductListPage.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import ProductList from "../components/Product/ProductList";
+import "../css/product/productlist.css";
 
 export default class ProductListPage extends Component {
+
     render() {
         return(
-            <div className="row container-fluid">
-                <ProductList/>
+            <div>
+                <h4 style={{ textAlign: "center" }}>Products</h4>
+                <div className="row col-md-12">
+                    <div className="col-md-2 left-nav-wrapper">
+                        Todo
+                    </div>
+                    <div className="col-md-10 product-grid">
+                        <ProductList/>
+                    </div>
+                </div>
             </div>
         )
     }
