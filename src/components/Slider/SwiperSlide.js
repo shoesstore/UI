af7d4e82b57765ba1da1f1af60503f7c8efb9f2b
phichidung <dungpc@sphinx-software.com>
2019-08-22 15:00:12 +0700

    add banner into home page

---
+++ src/components/Slider/SwiperSlide.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Swiper               from 'react-id-swiper';
+import '../../css/slide/slide.css';
+
+export default class SwiperSlide extends Component {
+    render() {
+
+        const params = {
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+            spaceBetween: 30
+        }
+
+        return (
+            <div className="swiper-slide-home">
+                <Swiper {...params}>
+                    <div><img src={require('../../image/slide/banner0.jpg')} alt="img" /></div>
+                    <div><img src={require('../../image/slide/banner1.jpg')} alt="img" /></div>
+                    <div><img src={require('../../image/slide/banner2.jpg')} alt="img" /></div>
+                    <div><img src={require('../../image/slide/banner3.jpg')} alt="img" /></div>
+                    <div><img src={require('../../image/slide/banner4.jpg')} alt="img" /></div>
+                </Swiper>
+            </div>
+        );
+    }
+}
