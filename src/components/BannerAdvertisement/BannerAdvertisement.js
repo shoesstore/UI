fa44843902ed3158d2d6af034b57ed91dc72dc5c
phichidung <dungpc@sphinx-software.com>
2019-08-22 10:04:30 +0700

    add gif into react

---
+++ src/components/BannerAdvertisement/BannerAdvertisement.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class BannerAdvertisement extends Component {
+
+    render() {
+
+
+        return (
+            <img src={require('../../image/gif/Advertisement.gif')}/>
+        );
+    }
+}
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component }     from 'react';
 import Carousel                 from "../components/Carousel/Carousel";
 import ProductList              from "../components/Product/ProductList";
+import BannerAdvertisement      from "../components/BannerAdvertisement/BannerAdvertisement";
 import                               '../css/carousel/carousel.css';
 
 
@@ -11,6 +12,7 @@ export default class HomePage extends Component {
             <div>
                 <Carousel/>
                 <ProductList/>
+                <BannerAdvertisement/>
             </div>
         );
     }
