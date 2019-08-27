4b06a73181302f6d0bc73d2621cbbd433dec4a15
LongHai97 <haitran6397@gmail.com>
2019-08-27 09:51:41 +0700

    fix the routes 404 page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import {Redirect, Route, Switch} from 'react-router-dom';
+import {Route, Switch }     from 'react-router-dom';
 import HomePage             from './containers/HomePage';
 import ProductListPage      from "./containers/ProductListPage";
 import ProductDetailPage    from "./containers/ProductDetailPage";
 import CartPage             from "./containers/CartPage";
-import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
+import NotFoundPage         from "./components/NotFoundPage/NotFoundPage";
 
 
 export default class RouterProtect extends Component {
@@ -12,12 +12,10 @@ export default class RouterProtect extends Component {
         return (
             <Switch>
                 <Route exact path="/"            component={HomePage}         />
-                <Route       path="*"            component={NotFoundPage}     />
-                <Route       path="/404"         component={NotFoundPage}     />
-                <Redirect    to  ="/404"                                        />
                 <Route exact path="/collections" component={ProductListPage}  />
                 <Route exact path="/product"     component={ProductDetailPage}/>
                 <Route exact path="/cart"        component={CartPage}         />
+                <Route exact path="*"            component={NotFoundPage}     />
             </Switch>
         )
     }
