78321f9e11f58c5d9a030189a8f6ba3979ed3f30
LongHai97 <haitran6397@gmail.com>
2019-08-27 17:00:17 +0700

    update page 404

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import                         "./notfoundpage.css"
 
 export default class NotFoundPage extends Component {
     render() {
-        return(
-          <div>
-
-          </div>
+        return (
+            <div className="container-fluid">
+                <div className="page-not-found">
+                </div>
+            </div>
         );
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, Switch }     from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import HomePage             from './containers/HomePage';
 import ProductListPage      from "./containers/ProductListPage";
 import ProductDetailPage    from "./containers/ProductDetailPage";
@@ -10,13 +10,15 @@ import NotFoundPage         from "./components/NotFoundPage/NotFoundPage";
 export default class RouterProtect extends Component {
     render() {
         return (
-            <Switch>
-                <Route exact path="/"            component={HomePage}         />
-                <Route exact path="/collections" component={ProductListPage}  />
-                <Route exact path="/product"     component={ProductDetailPage}/>
-                <Route exact path="/cart"        component={CartPage}         />
-                <Route exact path="*"            component={NotFoundPage}     />
-            </Switch>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/"            component={HomePage}         />
+                    <Route exact path="/collections" component={ProductListPage}  />
+                    <Route exact path="/product"     component={ProductDetailPage}/>
+                    <Route exact path="/cart"        component={CartPage}         />
+                    <Route       path="*"            component={NotFoundPage}     />
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
