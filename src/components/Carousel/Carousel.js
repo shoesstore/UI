c51169171d01d18bad3fa75bf7f730df88927917
phichidung <dungpc@sphinx-software.com>
2019-08-30 10:27:22 +0700

    re-commit

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -10,7 +10,7 @@ export default class Carousel extends Component {
             { src: require('../../image/gif/Advertisement.gif'), altText: 'Slide 0', caption: 'ShoesStore', header: 'Advertisement' },
             { src: require('../../image/carousel/1.jpg'), altText: 'Slice 1', caption: 'ShoesStore', header: 'Explosion Shoes' },
             { src: require('../../image/carousel/2.jpg'), altText: 'Slice 2', caption: 'ShoesStore', header: 'Neon Shoes' },
-            { src: require('../../image/carousel/3.jpg'), altText: 'Slice 3', caption: 'ShoesStore', heeader: 'Fire Shoes' },
+            { src: require('../../image/carousel/3.jpg'), altText: 'Slice 3', caption: 'ShoesStore', header: 'Fire Shoes' },
         ];
 
         return (
