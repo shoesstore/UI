5e06328e3f00719930a91ed506a6a38d87885136
phichidung <dungpc@sphinx-software.com>
2019-08-30 10:25:50 +0700

    add altText and caption and header in Carousel.js

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -7,10 +7,10 @@ export default class Carousel extends Component {
     render() {
 
         const items = [
-            { src: require('../../image/gif/Advertisement.gif'), altText: 'Slide 0', caption: 'Slide 0', header: 'Slide 0 Header' },
-            { src: require('../../image/carousel/1.jpg'), altText: 'Slice 1', caption: 'Slide 1', header: 'Slide 1 Header' },
-            { src: require('../../image/carousel/2.jpg'), altText: 'Slice 2', caption: 'Slide 2', header: 'Slide 2 Header' },
-            { src: require('../../image/carousel/3.jpg'), altText: 'Slice 3', caption: 'Slide 3', header: 'Slide 3 Header' },
+            { src: require('../../image/gif/Advertisement.gif'), altText: 'Slide 0', caption: 'ShoesStore', header: 'Advertisement' },
+            { src: require('../../image/carousel/1.jpg'), altText: 'Slice 1', caption: 'ShoesStore', header: 'Explosion Shoes' },
+            { src: require('../../image/carousel/2.jpg'), altText: 'Slice 2', caption: 'ShoesStore', header: 'Neon Shoes' },
+            { src: require('../../image/carousel/3.jpg'), altText: 'Slice 3', caption: 'ShoesStore', heeader: 'Fire Shoes' },
         ];
 
         return (
